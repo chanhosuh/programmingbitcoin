07ce827a8efda67fd4c18a7d62b774ca3dfc0515
Chan-Ho Suh <chanhosuh@gmail.com>
2019-03-25 16:21:16 -0400

    chapter 6 exercises (incomplete)

--- code-ch06/op.py
+++ code-ch06/op.py
@@ -647,7 +647,11 @@ def op_hash160(stack):
     # check that there's at least 1 element on the stack
     # pop off the top element from the stack
     # push a hash160 of the popped off element to the stack
-    raise NotImplementedError
+    if len(stack) < 1:
+        return False
+    element = stack.pop()
+    stack.append(hash160(element))
+    return True
 
 
 # tag::source2[]
@@ -668,8 +672,18 @@ def op_checksig(stack, z):
     # parse the serialized pubkey and signature into objects
     # verify the signature using S256Point.verify()
     # push an encoded 1 or 0 depending on whether the signature verified
-    raise NotImplementedError
+    if len(stack) < 2:
+        return False
+    public_key_bytes = stack.pop()
+    public_key = S256Point.parse(public_key_bytes)
 
+    signature_bytes = stack.pop()
+    signature_bytes = signature_bytes[:-1]
+    signature = Signature.parse(signature_bytes)
+
+    result = public_key.verify(z, signature)
+    stack.append(encode_num(int(result)))
+    return True
 
 def op_checksigverify(stack, z):
     return op_checksig(stack, z) and op_verify(stack)
